425b2a8aa0fbc932d23b41d298e95987702eae23
R. Lemos (oslo) <rslemos@gmail.com>
2013-09-29 14:33:04 -0300

    simplistic test case (Roche and Schabes 1995)
    Mainly adds test case infrastructure.

--- brilltagger/src/test/java/br/eti/rslemos/nlp/brill/BrillTaggerUnitTest.java
+++ brilltagger/src/test/java/br/eti/rslemos/nlp/brill/BrillTaggerUnitTest.java
@@ -21,12 +21,19 @@
  ******************************************************************************/
 package br.eti.rslemos.nlp.brill;
 
-import static org.mockito.Mockito.mock;
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.CoreMatchers.*;
+
 
 import org.junit.Before;
 import org.junit.Test;
 
+import br.eti.rslemos.nlp.DefaultSentence;
+import br.eti.rslemos.nlp.DefaultText;
+import br.eti.rslemos.nlp.DefaultToken;
+import br.eti.rslemos.nlp.Sentence;
 import br.eti.rslemos.nlp.Text;
+import br.eti.rslemos.nlp.Token;
 
 public class BrillTaggerUnitTest {
 	
@@ -39,7 +46,77 @@ public class BrillTaggerUnitTest {
 	
 	@Test
 	public void testEmptyTaggerOverEmptyText() {
-		Text text = mock(Text.class);
+		Text text = text();
 		tagger.tag(text);
+	}
+	
+	@Test
+	public void testExample3RocheAndSchabes1995() {
+		Text text = text("He/PPS witnessed/VBD Lennon/NP killed/VBN by/BY Chapman/NP");
+		tagger.tag(text);
+
+		verify(toStrings(text), "He/PPS witnessed/VBD Lennon/NP killed/VBN by/BY Chapman/NP");
+	}
+	
+	private static Text text(String... sentences) {
+		Text text = new DefaultText();
+		
+		for (String s : sentences) {
+			text.add(sentence(s));
+		}
+		
+		return text;
+	}
+	
+	private static Sentence sentence(String s) {
+		DefaultSentence sentence = new DefaultSentence();
+		
+		String[] tokens = s.split(" ");
+		for (String t : tokens) {
+			sentence.add(token(t));
+		}
+		
+		return sentence;
+	}
+
+	private static Token token(String t) {
+		DefaultToken token = new DefaultToken();
+		
+		String[] features = t.split("/", 2);
+		token.put("WORD", features[0]);
+		token.put("POS", features[1]);
+		
+		return token;
+	}
+	
+	private static String[] toStrings(Text text) {
+		String[] strings = new String[text.size()];
+		
+		for (int i = 0; i < strings.length; i++) {
+			strings[i] = toString(text.get(i));
+		}
+		
+		return strings;
+	}
+
+	private static String toString(Sentence sentence) {
+		StringBuilder s = new StringBuilder();
+		
+		for (Token token : sentence) {
+			s.append(toString(token)).append(' ');
+		}
+		
+		if (s.length() > 1)
+			s.setLength(s.length() - 1);
+		
+		return s.toString();
+	}
+
+	private static String toString(Token token) {
+		return token.get("WORD") + "/" + token.get("POS");
+	}
+
+	private static void verify(String[] actual, String... expected) {
+		assertThat(actual, is(equalTo(expected)));
 	}
 }
