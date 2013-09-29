aa331bbaf77e2b39108a01ed2c85f27d8b351e49
R. Lemos (oslo) <rslemos@gmail.com>
2013-09-29 15:28:39 -0300

    straight forward implementation of brill tagger

--- brilltagger/src/main/java/br/eti/rslemos/nlp/brill/BrillTagger.java
+++ brilltagger/src/main/java/br/eti/rslemos/nlp/brill/BrillTagger.java
@@ -21,11 +21,64 @@
  ******************************************************************************/
 package br.eti.rslemos.nlp.brill;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import br.eti.rslemos.nlp.Sentence;
 import br.eti.rslemos.nlp.Tagger;
 import br.eti.rslemos.nlp.Text;
+import br.eti.rslemos.nlp.Token;
 
 public class BrillTagger implements Tagger {
 
+	private List<Rule> rules = new ArrayList<Rule>();
+
 	public void tag(Text text) {
+		for (Rule rule : rules) {
+			apply(rule, text);
+		}
+	}
+
+	public void addRule(Rule rule) {
+		rules.add(rule);
+	}
+	
+	private static void apply(Rule rule, Text text) {
+		for (Sentence sentence : text) {
+			apply(rule, sentence);
+		}
+	}
+
+	private static void apply(Rule rule, Sentence sentence) {
+		for (int i = 0; i < sentence.size(); i++) {
+			apply(rule, sentence, i);
+		}
+	}
+
+	private static void apply(Rule rule, Sentence sentence, int i) {
+		for (int j = 0; j < rule.matches.length; j++) {
+			int real = Rule.stored2Real(j) + i;
+			
+			if (real >= 0 && real < sentence.size()) {
+				if (rule.matches[j] != null && !match(rule.matches[j], sentence.get(real))) 
+					return;
+			}
+		}
+		
+		sentence.get(i).putAll(rule.sets);
+	}
+
+	private static boolean match(Map<String, Object> match, Token token) {
+		for (Map.Entry<String, Object> entry : match.entrySet()) {
+			if (!token.containsKey(entry.getKey()) || !eq(entry.getValue(), token.get(entry.getKey())))
+				return false;
+		}
+		
+		return true;
+	}
+
+	private static boolean eq(Object o1, Object o2) {
+		return o1 != null ? o1.equals(o2) : o2 == null;
 	}
 }
--- brilltagger/src/test/java/br/eti/rslemos/nlp/brill/BrillTaggerUnitTest.java
+++ brilltagger/src/test/java/br/eti/rslemos/nlp/brill/BrillTaggerUnitTest.java
@@ -24,7 +24,6 @@ package br.eti.rslemos.nlp.brill;
 import static org.junit.Assert.assertThat;
 import static org.hamcrest.CoreMatchers.*;
 
-
 import org.junit.Before;
 import org.junit.Test;
 
@@ -51,13 +50,51 @@ public class BrillTaggerUnitTest {
 	}
 	
 	@Test
+	public void testExampleMarkHepple2000() {
+		tagger.addRule(PREVTAG("NN", "VB", "TO"));
+		tagger.addRule(WDPREVTAG("RB", "RP", "VB", "up"));
+		
+		Text text = text("to/TO sign/NN up/RB");
+		tagger.tag(text);
+
+		verify(toStrings(text), "to/TO sign/VB up/RP");
+	}
+	
+	@Test
+	public void testExample1RocheAndSchabes1995() {
+		addRocheAndSchabes1995Rules();
+		
+		Text text = text("Chapman/NP killed/VBN John/NP Lennon/NP");
+		tagger.tag(text);
+		
+		verify(toStrings(text), "Chapman/NP killed/VBD John/NP Lennon/NP");
+	}
+
+	@Test
+	public void testExample2RocheAndSchabes1995() {
+		addRocheAndSchabes1995Rules();
+		
+		Text text = text("John/NP Lennon/NP was/BEDZ shot/VBD by/BY Chapman/NP");
+		tagger.tag(text);
+
+		verify(toStrings(text), "John/NP Lennon/NP was/BEDZ shot/VBN by/BY Chapman/NP");
+	}
+
+	@Test
 	public void testExample3RocheAndSchabes1995() {
+		addRocheAndSchabes1995Rules();
+		
 		Text text = text("He/PPS witnessed/VBD Lennon/NP killed/VBN by/BY Chapman/NP");
 		tagger.tag(text);
 
 		verify(toStrings(text), "He/PPS witnessed/VBD Lennon/NP killed/VBN by/BY Chapman/NP");
 	}
 	
+	private void addRocheAndSchabes1995Rules() {
+		tagger.addRule(PREVTAG("VBN", "VBD", "NP"));
+		tagger.addRule(NEXTTAG("VBD", "VBN", "BY"));
+	}
+
 	private static Text text(String... sentences) {
 		Text text = new DefaultText();
 		
@@ -119,4 +156,39 @@ public class BrillTaggerUnitTest {
 	private static void verify(String[] actual, String... expected) {
 		assertThat(actual, is(equalTo(expected)));
 	}
+	
+	private static Rule PREVTAG(String fromPOS, String toPOS, String prevPOS) {
+		Rule rule = new Rule();
+		
+		rule.addMatch( 0, "POS", fromPOS);
+		rule.addMatch(-1, "POS", prevPOS);
+		
+		rule.addSet(0, "POS", toPOS);
+		
+		return rule;
+	}
+
+	private static Rule NEXTTAG(String fromPOS, String toPOS, String nextPOS) {
+		Rule rule = new Rule();
+		
+		rule.addMatch( 0, "POS", fromPOS);
+		rule.addMatch( 1, "POS", nextPOS);
+		
+		rule.addSet(0, "POS", toPOS);
+		
+		return rule;
+	}
+
+	private static Rule WDPREVTAG(String fromPOS, String toPOS, String prevPOS, String thisWORD) {
+		Rule rule = new Rule();
+		
+		rule.addMatch( 0, "POS", fromPOS);
+		rule.addMatch( 0, "WORD", thisWORD);
+		rule.addMatch(-1, "POS", prevPOS);
+		
+		rule.addSet(0, "POS", toPOS);
+		
+		return rule;
+	}
+
 }
